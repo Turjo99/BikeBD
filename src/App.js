94c94d07acbceaef3ba0049e6ff120ed231175ba
Turjo99 <imturjo27@gmail.com>
2022-12-26 22:53:17 +0600

    Routes created for all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./Components/Main";
+import Home from "./Components/Home";
+import Banner from "./Components/Banner";
+import Services from "./Components/Services";
+import Contact from "./Components/Contact";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+        {
+          path: "banner",
+          element: <Banner></Banner>,
+        },
+        {
+          path: "services",
+          element: <Services></Services>,
+        },
+        {
+          path: "contact",
+          element: <Contact></Contact>,
+        },
+      ],
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
   );
 }
 
---
+++ src/Components/Services.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <div>
+      <h1 className=" font-bold text-5xl my-5 text-center">Our Products</h1>
+    </div>
+  );
+};
+
+export default Services;
